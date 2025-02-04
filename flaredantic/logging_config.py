e29c01e7fa1c051cfee02c0c110483ff9efbb33d
linuztx <linuztx@gmail.com>
2025-02-04 17:34:19 +0800

    Update log

--- flaredantic/logging_config.py
+++ flaredantic/logging_config.py
@@ -4,13 +4,23 @@ import logging
 GREEN = "\033[92m"
 RESET = "\033[0m"
 
+# Global logger instance
+_logger = None
+
 def setup_logger(verbose: bool = False) -> logging.Logger:
     """Configure and return a logger with custom formatting"""
-    logger = logging.getLogger("flaredantic")
-    logger.setLevel(logging.DEBUG if verbose else logging.INFO)
+    global _logger
+    
+    # Return existing logger if already configured
+    if _logger is not None:
+        _logger.setLevel(logging.DEBUG if verbose else logging.INFO)
+        return _logger
+    
+    _logger = logging.getLogger("flaredantic")
+    _logger.setLevel(logging.DEBUG if verbose else logging.INFO)
     
     # Remove existing handlers
-    logger.handlers.clear()
+    _logger.handlers.clear()
     
     # Create console handler with custom formatting
     console_handler = logging.StreamHandler()
@@ -22,6 +32,9 @@ def setup_logger(verbose: bool = False) -> logging.Logger:
         datefmt='%Y-%m-%d %H:%M:%S'
     )
     console_handler.setFormatter(formatter)
-    logger.addHandler(console_handler)
+    _logger.addHandler(console_handler)
     
-    return logger 
+    # Log GitHub URL and version
+    _logger.info("https://github.com/linuztx/flaredantic")
+    
+    return _logger 
